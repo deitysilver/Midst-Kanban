9babf8a243fa4ecff7d8366345b438a16ec443bf
theonesilverest <theonesilverest@gmail.com>
2024-09-30 18:05:35 +0000

    Project Initialized

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { createSignal } from 'solid-js'
+import './App.css'
+
+function App() {
+  const [count, setCount] = createSignal(0)
+
+  return (
+    <div class="">
+      
+    </div>
+  )
+}
+
+export default App
